86e85f62505d67ffc9db68a423abdd090f211c4b
FruitWave <league@WTS8.attlocal.net>
2017-10-21 13:58:20 -0700

    finished exam

--- src/ColorButtons.java
+++ src/ColorButtons.java
@@ -5,59 +5,67 @@ import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+
 public class ColorButtons implements MouseListener {
-public static void main(String[] args) {
-ColorButtons nag= new ColorButtons();
-}
-ColorButtons(){
-JFrame jframe= new JFrame();
-JButton redbutton= new JButton();
-JButton greenbutton= new JButton();
-JButton yellowbutton= new JButton();
-JButton bluebutton= new JButton();
-JPanel jpanel= new JPanel();
-jframe.setVisible(true);
-jframe.setSize(500,500);
-jframe.add(jpanel);
-jpanel.add(redbutton);
-jpanel.add(greenbutton);
-jpanel.add(yellowbutton);
-jpanel.add(bluebutton);
-redbutton.setBackground(Color.RED);
-redbutton.setOpaque(true);
-greenbutton.setBackground(Color.GREEN);
-greenbutton.setOpaque(true);
-bluebutton.setBackground(Color.BLUE);
-bluebutton.setOpaque(true);
-yellowbutton.setBackground(Color.YELLOW);
-yellowbutton.setOpaque(true);
-}
-
-void speak(String words) {
-	try {
-		Runtime.getRuntime().exec("say" + words);
-	}catch (IOException e) {
-		e.printStackTrace();
+	public static void main(String[] args) {
+		MouseEvent MouseEvent;
+		ColorButtons nag = new ColorButtons(MouseEvent);
 	}
+
+
+	ColorButtons(MouseEvent MouseEvent) {
+		JFrame jframe = new JFrame();
+		JButton redbutton = new JButton();
+		JButton greenbutton = new JButton();
+		JButton yellowbutton = new JButton();
+		JButton bluebutton = new JButton();
+		JPanel jpanel = new JPanel();
+		jframe.setVisible(true);
+		jframe.setSize(500, 500);
+		jframe.add(jpanel);
+		jpanel.add(redbutton);
+		jpanel.add(greenbutton);
+		jpanel.add(yellowbutton);
+		jpanel.add(bluebutton);
+		redbutton.setBackground(Color.RED);
+		redbutton.setOpaque(true);
+		greenbutton.setBackground(Color.GREEN);
+		greenbutton.setOpaque(true);
+		bluebutton.setBackground(Color.BLUE);
+		bluebutton.setOpaque(true);
+		yellowbutton.setBackground(Color.YELLOW);
+		yellowbutton.setOpaque(true);
+		mousePressed(MouseEvent);
+		}
+	
+		void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say" + words);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		}
+		
 }
-@Override
-public void mouseClicked(MouseEvent e) {
+
+		
+		public void mousePressed(MouseEvent e, yellowbutton, greenbutton, redbutton, bluebutton) {
+			e.getSource();	
+			if((yellowbutton=e.getSource())) {
+				speak("yellow");
+			}
+			else if(greenbutton=e.getSource()){
+				speak("green");
+			}
+			else if(redbutton=e.getSource()) {
+				speak("red");
+			}
+			else if(bluebutton=e.getSource()) {
+				speak("blue");
+			}
+			
+		}
 
 	
-}
-public void mousePressed(MouseEvent e) {
-	
-	
-}
-public void mouseReleased(MouseEvent e) {
-	
-	
-}
-public void mouseEntered(MouseEvent e) {
-	
-	
-}
-public void mouseExited(MouseEvent e) {
-	
-}
-}
